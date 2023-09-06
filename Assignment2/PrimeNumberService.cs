64bb861b2af41ff5c36090a10cbf8abb2c12b6ef
AshishReddyExecideas <ashish@execideas.com>
2023-09-06 14:15:04 +0530

    Pushing code of generating random prime number

---
+++ Assignment2/PrimeNumberService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class PrimeNumberService
+    {
+        private int minValue;
+        private int maxValue;
+        public Dictionary<int,bool> dict = new Dictionary<int,bool>();
+
+        
+        internal void CreateRandomRange()
+        {
+            bool hasOldRange = minValue > 0 && maxValue > 0;
+            Random rnd = new Random();
+            if (!hasOldRange)
+            {
+                minValue = rnd.Next();
+            }
+            else
+            {
+                minValue = rnd.Next(maxValue, int.MaxValue);
+            }
+
+            maxValue = rnd.Next(minValue + 1000, int.MaxValue);
+        }
+
+        internal int GenerateRandomPrimeNumber()
+        {
+            CreateRandomRange();
+            for (int i = minValue; i <= maxValue; i++) 
+            {
+                if (IsItPrime(i))
+                    return i;
+            }
+
+            return GenerateRandomPrimeNumber();
+        }
+
+        internal bool IsItPrime(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+
+            if (n == 2 || n == 3)
+            {
+                return true;
+            }
+            else if (n % 6 == 1 || n % 6 == 5)
+            {
+                var sqrt = Math.Sqrt(n);
+                for (int i = 2; i <= sqrt; i++)
+                {
+                    if ((n % i) == 0)
+                        return false;
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        internal void GenerateRandomPrimesandStore()
+        {
+            CreateRandomRange();
+            for (int i = minValue; i <= maxValue; i++)
+            {
+                if(IsItPrime(i))
+                {
+                    dict.Add(i,true);
+                }
+                else
+                {
+                    dict.Add(i,false);
+                }
+            }
+        }
+    }
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Assignment2;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        PrimeNumberService p = new PrimeNumberService();
+        for (int i = 0; i < 5; i++)
+        {
+            var prime = p.GenerateRandomPrimeNumber();
+            Console.WriteLine(prime);
+        }
+
+        Console.WriteLine("__________________\n");
+
+        p.GenerateRandomPrimesandStore();
+        foreach (var item in p.dict)
+        {
+            if (item.Value == true)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+    }
+}
